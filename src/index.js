e5836cd64b6bfc20e348c35bf421c117182220a3
Bryan Miller <bmilcs@yahoo.com>
2023-01-01 13:08:21 -0500

    Remove router import from index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Routes from "./Router";
 import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
