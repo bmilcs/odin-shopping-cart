3c144ac3f6141540705368295b99ce8743b98288
Bryan Miller <bmilcs@yahoo.com>
2023-01-01 17:35:45 -0500

    Refactor & style product card components

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,6 @@
 import React from "react";
-
-import AddShoppingCartIcon from "../assets/AddShoppingCartIcon";
+import Button from "./Button";
+import "../styles/ProductCard.scss";
 
 function ProductCard({
   itemDetails: {
@@ -13,12 +13,16 @@ function ProductCard({
 }) {
   return (
     <div className="product-card">
-      <h2>{title}</h2>
-      <h3>${price}</h3>
-      <AddShoppingCartIcon />
-      <p>{rate} / 5.0 Stars</p>
-      <p>{description}</p>
-      <img src={image} alt={title} />
+      <div className="image-wrapper">
+        <img className="product-image" src={image} alt={title} />
+      </div>
+      <h2 className="product-name">{title}</h2>
+      <p className="product-rating">{rate} / 5.0 Stars</p>
+      <p className="product-description">{description}</p>
+      <div className="price-button-wrapper">
+        <h3 className="product-price">${price}</h3>
+        <Button>Add To Cart</Button>
+      </div>
     </div>
   );
 }
