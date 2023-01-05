7180f3745c6741b6747c5c5d59cb5b562be0c465
Bryan Miller <bmilcs@yahoo.com>
2023-01-05 16:27:28 -0500

    Add order summary section

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "../styles/Cart.scss";
 
 function Cart({
@@ -7,9 +8,40 @@ function Cart({
   onIncrementQuantity,
   onDecrementQuantity,
 }) {
+  const [subTotal, setSubTotal] = useState(0);
+  const [shipping, setShipping] = useState(0);
+  const [taxes, setTaxes] = useState(0);
+  const [orderTotal, setOrderTotal] = useState(0);
+
+  useEffect(() => {
+    setSubTotal(
+      Object.keys(cart)
+        .reduce((acc, cur) => {
+          return acc + cart[cur].price * cart[cur].quantity;
+        }, 0)
+        .toFixed(2)
+    );
+  }, [cart]);
+
+  useEffect(() => {
+    setShipping((cartQty * 3.5).toFixed(2));
+  }, [subTotal]);
+
+  useEffect(() => {
+    setTaxes((subTotal * 0.05).toFixed(2));
+  }, [shipping]);
+
+  useEffect(() => {
+    setOrderTotal((+subTotal + +taxes + +shipping).toFixed(2));
+  }, [taxes]);
+
+  console.count("render");
+
   return (
     <div className="inside">
       <h1>Cart</h1>
+
+      {/* shopping cart items, w/ -/+ buttons for changing qty*/}
       {!cartQty ? (
         <p>No items are present in your shopping cart</p>
       ) : (
@@ -17,14 +49,12 @@ function Cart({
           {Object.keys(cart).map((productID) => {
             const product = cart[productID];
             return (
-              // <div className="cart-product-wrapper" key={productID}>
               <>
                 <img
                   className="cart-product-image"
                   src={product.image}
                   alt=""
                 />
-                {/* <div className="product-details-wrapper"> */}
                 <p className="cart-product-title">{product.title}</p>
                 <div className="cart-quantity-wrapper">
                   <button
@@ -56,13 +86,27 @@ function Cart({
                   <p className="cart-product-price-total">
                     ${(product.price * product.quantity).toFixed(2)}
                   </p>
-                  {/* </div> */}
                 </div>
               </>
             );
           })}
         </div>
       )}
+
+      {/* order summary */}
+      <div className="order-summary-container">
+        <h2 className="order-summary-title">Order Summary</h2>
+        <div className="order-summary-wrapper">
+          <p className="order-summary-label">Subtotal:</p>
+          <p className="order-subtotal">${subTotal}</p>
+          <p className="order-summary-label">Taxes:</p>
+          <p className="order-taxes">${taxes}</p>
+          <p className="order-summary-label">Shipping:</p>
+          <p className="order-shipping">${shipping}</p>
+          <p className="order-summary-label order-total-label">Total:</p>
+          <p className="order-total">${orderTotal}</p>
+        </div>
+      </div>
     </div>
   );
 }
