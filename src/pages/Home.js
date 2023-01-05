8a2d580fe462e4560b4c6fa7ed8c40a24cb16686
Bryan Miller <bmilcs@yahoo.com>
2023-01-05 17:33:44 -0500

    Add home page content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import splashImg from "../assets/splash-photo.jpg";
+import "../styles/Home.scss";
 function Home() {
   return (
-    <div className="inside">
-      <h1>Home</h1>
+    <div className="home-hero-section">
+      <div className="inside">
+        <div className="hero-text-wrapper">
+          <h1 className="hero-title">Be the envy of all</h1>
+          <p className="hero-paragraph">
+            Discover a world of endless possibilities at our online store! From
+            sparkling jewelry to the latest electronics, and fashionable
+            clothing for men and women, we have something for every taste and
+            budget. Come explore and be inspired by our wide selection.
+          </p>
+          <a href="/shop">
+            <button className="hero-button">Shop Now</button>
+          </a>
+        </div>
+        <img
+          src={splashImg}
+          className="home-hero-image"
+          alt="Happy woman giving peace signs"
+        />
+      </div>
     </div>
   );
 }
