cbef649ab10486bf6652dd252bb884fb29766af7
Bryan Miller <bmilcs@yahoo.com>
2023-01-06 15:55:56 -0500

    Add basename prop to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/odin-shopping-cart">
       <App />
     </BrowserRouter>
   </React.StrictMode>
