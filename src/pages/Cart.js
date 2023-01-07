eaa971bdd5e35a95031baf07d4d4d30d75e0ba9c
Bryan Miller <bmilcs@yahoo.com>
2023-01-07 15:52:23 -0500

    Upgrade empty cart with image & button

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/Cart.scss";
+import emptyCartImg from "../assets/empty-cart.jpg";
 
 function Cart({
   cart,
@@ -36,74 +38,80 @@ function Cart({
 
   return (
     <div className="inside">
-      <h1>Cart</h1>
-
       {/* shopping cart items, w/ -/+ buttons for changing qty*/}
       {!cartQty ? (
-        <p>No items are present in your shopping cart</p>
+        <div className="empty-shopping-cart-wrapper">
+          <img className="empty-cart-image" src={emptyCartImg} alt="" />
+          <Link to="/shop">
+            <button className="empty-cart-button">Fill me up!</button>
+          </Link>
+        </div>
       ) : (
-        <div className="shopping-cart-wrapper">
-          {Object.keys(cart).map((productID) => {
-            const product = cart[productID];
-            return (
-              <>
-                <img
-                  className="cart-product-image"
-                  src={product.image}
-                  alt=""
-                />
-                <p className="cart-product-title">{product.title}</p>
-                <div className="cart-quantity-wrapper">
-                  <button
-                    className="quantity-button"
-                    data-product_id={product.id}
-                    onClick={onDecrementQuantity}
-                  >
-                    -
-                  </button>
-                  <input
-                    id={`${product.id}-quantity`}
-                    className="cart-product-quantity"
-                    data-product_id={product.id}
-                    value={product.quantity}
-                    onChange={onQuantityChange}
+        // </div>
+        <>
+          <h1>Cart</h1>
+          <div className="shopping-cart-wrapper">
+            {Object.keys(cart).map((productID) => {
+              const product = cart[productID];
+              return (
+                <>
+                  <img
+                    className="cart-product-image"
+                    src={product.image}
+                    alt=""
                   />
-                  <button
-                    className="quantity-button"
-                    data-product_id={product.id}
-                    onClick={onIncrementQuantity}
-                  >
-                    +
-                  </button>
-                </div>
-                <div className="product-numbers-wrapper">
-                  <p className="cart-product-price-unit">
-                    ${product.price.toFixed(2)}
-                  </p>
-                  <p className="cart-product-price-total">
-                    ${(product.price * product.quantity).toFixed(2)}
-                  </p>
-                </div>
-              </>
-            );
-          })}
-        </div>
+                  <p className="cart-product-title">{product.title}</p>
+                  <div className="cart-quantity-wrapper">
+                    <button
+                      className="quantity-button"
+                      data-product_id={product.id}
+                      onClick={onDecrementQuantity}
+                    >
+                      -
+                    </button>
+                    <input
+                      id={`${product.id}-quantity`}
+                      className="cart-product-quantity"
+                      data-product_id={product.id}
+                      value={product.quantity}
+                      onChange={onQuantityChange}
+                    />
+                    <button
+                      className="quantity-button"
+                      data-product_id={product.id}
+                      onClick={onIncrementQuantity}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div className="product-numbers-wrapper">
+                    <p className="cart-product-price-unit">
+                      ${product.price.toFixed(2)}
+                    </p>
+                    <p className="cart-product-price-total">
+                      ${(product.price * product.quantity).toFixed(2)}
+                    </p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+          {/* order summary */}
+          <div className="order-summary-container">
+            <h2 className="order-summary-title">Order Summary</h2>
+            <div className="order-summary-wrapper">
+              <p className="order-summary-label">Subtotal:</p>
+              <p className="order-subtotal">${orderSummary.subtotal}</p>
+              <p className="order-summary-label">Taxes:</p>
+              <p className="order-taxes">${orderSummary.taxes}</p>
+              <p className="order-summary-label">Shipping:</p>
+              <p className="order-shipping">${orderSummary.shipping}</p>
+              <p className="order-summary-label order-total-label">Total:</p>
+              <p className="order-total">${orderSummary.total}</p>
+            </div>
+          </div>
+        </>
       )}
-
-      {/* order summary */}
-      <div className="order-summary-container">
-        <h2 className="order-summary-title">Order Summary</h2>
-        <div className="order-summary-wrapper">
-          <p className="order-summary-label">Subtotal:</p>
-          <p className="order-subtotal">${orderSummary.subtotal}</p>
-          <p className="order-summary-label">Taxes:</p>
-          <p className="order-taxes">${orderSummary.taxes}</p>
-          <p className="order-summary-label">Shipping:</p>
-          <p className="order-shipping">${orderSummary.shipping}</p>
-          <p className="order-summary-label order-total-label">Total:</p>
-          <p className="order-total">${orderSummary.total}</p>
-        </div>
-      </div>
     </div>
   );
 }
