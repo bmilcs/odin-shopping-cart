0a309e39876efc27808131e8a6f313aa55483a79
Bryan Miller <bmilcs@yahoo.com>
2023-01-07 17:59:39 -0500

    Fix styling issues across all pages

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -47,67 +47,67 @@ function Cart({
           </Link>
         </div>
       ) : (
-        // </div>
         <>
-          <h1>Cart</h1>
-          <div className="shopping-cart-wrapper">
-            {Object.keys(cart).map((productID) => {
-              const product = cart[productID];
-              return (
-                <>
-                  <img
-                    className="cart-product-image"
-                    src={product.image}
-                    alt=""
-                  />
-                  <p className="cart-product-title">{product.title}</p>
-                  <div className="cart-quantity-wrapper">
-                    <button
-                      className="quantity-button"
-                      data-product_id={product.id}
-                      onClick={onDecrementQuantity}
-                    >
-                      -
-                    </button>
-                    <input
-                      id={`${product.id}-quantity`}
-                      className="cart-product-quantity"
-                      data-product_id={product.id}
-                      value={product.quantity}
-                      onChange={onQuantityChange}
-                    />
-                    <button
-                      className="quantity-button"
-                      data-product_id={product.id}
-                      onClick={onIncrementQuantity}
-                    >
-                      +
-                    </button>
-                  </div>
-                  <div className="product-numbers-wrapper">
-                    <p className="cart-product-price-unit">
-                      ${product.price.toFixed(2)}
-                    </p>
-                    <p className="cart-product-price-total">
-                      ${(product.price * product.quantity).toFixed(2)}
-                    </p>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-          {/* order summary */}
-          <div className="order-summary-container">
-            <h2 className="order-summary-title">Order Summary</h2>
-            <div className="order-summary-wrapper">
-              <p className="order-summary-label">Subtotal:</p>
-              <p className="order-subtotal">${orderSummary.subtotal}</p>
-              <p className="order-summary-label">Taxes:</p>
-              <p className="order-taxes">${orderSummary.taxes}</p>
-              <p className="order-summary-label">Shipping:</p>
-              <p className="order-shipping">${orderSummary.shipping}</p>
-              <p className="order-summary-label order-total-label">Total:</p>
-              <p className="order-total">${orderSummary.total}</p>
+          <div className="cart-page-wrapper">
+            <h1 className="cart-title">Shopping Cart</h1>
+            <div className="shopping-cart-wrapper">
+              {Object.keys(cart).map((productID) => {
+                const product = cart[productID];
+                return (
+                  <>
+                    <div className="cart-product-wrapper">
+                      <img
+                        className="cart-product-image"
+                        src={product.image}
+                        alt=""
+                      />
+                      <p className="cart-product-title">{product.title}</p>
+                      <div className="cart-quantity-wrapper">
+                        <button
+                          className="quantity-button"
+                          data-product_id={product.id}
+                          onClick={onDecrementQuantity}
+                        >
+                          -
+                        </button>
+                        <input
+                          id={`${product.id}-quantity`}
+                          className="cart-product-quantity"
+                          data-product_id={product.id}
+                          value={product.quantity}
+                          onChange={onQuantityChange}
+                        />
+                        <button
+                          className="quantity-button"
+                          data-product_id={product.id}
+                          onClick={onIncrementQuantity}
+                        >
+                          +
+                        </button>
+                      </div>
+                      <p className="cart-product-price-unit">
+                        ${product.price.toFixed(2)}
+                      </p>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+
+            {/* order summary */}
+            <div className="order-summary-container">
+              <h2 className="order-summary-title">Order Summary</h2>
+              <div className="order-summary-wrapper">
+                <p className="order-summary-label">Subtotal:</p>
+                <p className="order-subtotal">${orderSummary.subtotal}</p>
+                <p className="order-summary-label">Taxes:</p>
+                <p className="order-taxes">${orderSummary.taxes}</p>
+                <p className="order-summary-label">Shipping:</p>
+                <p className="order-shipping">${orderSummary.shipping}</p>
+                <hr className="order-summary-line"></hr>
+                <p className="order-summary-label order-total-label">Total:</p>
+                <p className="order-total">${orderSummary.total}</p>
+              </div>
             </div>
           </div>
         </>
